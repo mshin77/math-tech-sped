fd3b1e05f125332c5ae058262b0185f59d701535
Mikyung Shin <39714607+mshin77@users.noreply.github.com>
2022-11-20 22:28:19 -0600

    update

--- scripts/preprocess.all.R
+++ scripts/preprocess.all.R
@@ -50,7 +50,8 @@ preprocess.all <- function(data, text_col = text_col, ...) {
         mutate(study_number = row_number()) %>% 
         ungroup()
     
-    write.csv()(united_tbl, file = "data/united_tbl.csv", colNames = TRUE)
+    library(readr)
+    write_csv(united_tbl, file = "data/united_tbl.csv")
     
     # Preprocess data 
     # Construct a corpus.
